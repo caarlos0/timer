8664b14d94d7e6039f47d1abb07d098a58ce9f62
yawaaaa <github1369@gmail.com>
2025-08-17 21:34:11 +0800

    Delete scripts directory

--- scripts/completions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-rm -rf completions
-mkdir completions
-go build -o timer .
-for sh in bash zsh fish; do
-	./timer completion "$sh" >"completions/timer.$sh"
-done
--- scripts/manpages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-rm -rf manpages
-mkdir manpages
-go run . man | gzip -c >manpages/timer.1.gz
